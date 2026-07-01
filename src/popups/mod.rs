1a3c0997c5aa532c50cbe0f0671919e2853075ec
Luis Ortiz <70501251+KiryuuLight@users.noreply.github.com>
2026-07-01 02:47:26 -0500

    feat: add the Overlay module

---
+++ src/popups/mod.rs
@@ -0,0 +1,17 @@
+mod add_task;
+mod alert;
+mod confirm_clear;
+mod confirm_delete;
+mod confirm_run;
+mod edit;
+mod help;
+mod prompt;
+
+pub use add_task::AddTask;
+pub use alert::Alert;
+pub use confirm_clear::ConfirmClearFilters;
+pub use confirm_delete::ConfirmDelete;
+pub use confirm_run::ConfirmRun;
+pub use edit::SectionEdit;
+pub use help::Help;
+pub use prompt::Prompt;
